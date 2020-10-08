5ccf4aac233188da4a9cccb11b83e093220d38e8
jun_q <jun_q@localhost>
2020-10-08 17:20:14 +0900

    20/10/08 Class Note

--- src/Day019_P/Day01902기본이론_P.java
+++ src/Day019_P/Day01902기본이론_P.java
@@ -1,32 +1,32 @@
-package Day019_P;
-
-public class Day01902기본이론_P {
-	public static void main(String[] args) {
-
-		// 선언과 초기화
-		int x;
-		x = 10;
-		System.out.println(x);
-		int y = 20;
-		System.out.println(y);
-
-		// 자료형
-		/*
-		 * byte short int long float double char boolean
-		 */
-
-		// 참조형
-		int[] arr = new int[5];
-		System.out.println(arr);
-
-		// 알파벳 소문자 a부터 z까지 출력해보기
-		// hint) 아스키코드
-		char alhapet = 'a';
-		//		System.out.println(a);
-		//		System.out.println((char) (a + 1));
-		//		System.out.println((char) (a + 2));
-		for (int i = 0; i < 20; i++) {
-			System.out.print((char) (alhapet + i) + " ");
+	package Day019_P;
+	
+	public class Day01902기본이론_P {
+		public static void main(String[] args) {
+	
+			// 선언과 초기화
+			int x;
+			x = 10;
+			System.out.println(x);
+			int y = 20;
+			System.out.println(y);
+	
+			// 자료형
+			/*
+			 * byte short int long float double char boolean
+			 */
+	
+			// 참조형
+			int[] arr = new int[5];
+			System.out.println(arr);
+	
+			// 알파벳 소문자 a부터 z까지 출력해보기
+			// hint) 아스키코드
+			char alhapet = 'a';
+			//		System.out.println(a);
+			//		System.out.println((char) (a + 1));
+			//		System.out.println((char) (a + 2));
+			for (int i = 0; i < 20; i++) {
+				System.out.print((char) (alhapet + i) + " ");
+			}
 		}
 	}
-}
--- src/Day019_P/Day01905기본이론_P.java
+++ src/Day019_P/Day01905기본이론_P.java
@@ -5,21 +5,21 @@ import java.util.Scanner;
 public class Day01905기본이론_P {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
-		for(int i = 1; i<=3;i++) {
-			for(int j=1;j<=3;j++) {
+
+		for (int i = 1; i <= 3; i++) {
+			for (int j = 1; j <= 3; j++) {
 				System.out.println(i + " : " + j);
 				// j가 2일 경우 break를 통해 
-				if(j == 2) {
+				if (j == 2) {
 					break;
 				}
 			}
 		}
 		System.out.println("숫자 입력: ");
 		// -100 입력시 반복문 종료
-		while(true) {
+		while (true) {
 			int num = sc.nextInt();
-			if(num == -100) {
+			if (num == -100) {
 				System.out.println("프로그램 종료");
 				break;
 			}
@@ -27,4 +27,4 @@ public class Day01905기본이론_P {
 		}
 		sc.close();
 	}
-}	
+}
