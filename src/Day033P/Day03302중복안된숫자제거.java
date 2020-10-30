ec49d1ceec58fe53fc7b82d40be1f8de32ab06d0
jun_q <junlee365206>
2020-10-30 22:03:44 +0900

    delete array

--- src/Day033P/Day03302중복안된숫자제거.java
+++ src/Day033P/Day03302중복안된숫자제거.java
@@ -3,94 +3,40 @@ package Day033P;
 import java.util.Arrays;
 
 public class Day03302중복안된숫자제거 {
-    int[] test1 = {1, 2, 3, 2, 1};
-    int[] test2 = {1, 3, 4, 4, 2};
-    int[] test3 = {1, 1, 1, 2, 1};
-    int sizeofTest = test1.length;
+    public int[] test1 = {1, 2, 3, 2, 1};
+    public int[] test2 = {1, 3, 4, 4, 2};
+    public int[] test3 = {1, 1, 1, 2, 1};
+    public int sizeofTest = test1.length; // test array's length
 
     // 중복안된 숫자를 찾기
     // 그자리 인덱스 찾기
     // 배열 재설정
     // 다시 추가 후 표현
-    public void test1CheckArray() {
+    public void testCheckArray(int[] test) {
         int index = -1;
         for (int i = 0; i < sizeofTest; i++) {
             int count = 0;
             for (int j = 0; j < sizeofTest; j++) {
-                if (test1[i] == test1[j]) {
+                if (test[i] == test[j]) {
                     count++;
                 }
             }
-            if (count < 2) {
+            if (count < 2) { // if test[i] doesn't have same value
                 index = i;
             }
         }
         System.out.println(index); // cheat key
-        int[] temp = test1;
-        test1 = new int[4];
+        // vector
+        int[] temp = test;
+        test = new int[4];
         int indexCount = 0;
         for (int i = 0; i < sizeofTest; i++) {
             if (i != index) {
-                test1[indexCount] = temp[i];
+                test[indexCount] = temp[i];
                 indexCount++;
             }
         }
-    }
-
-    public void test2CheckArray() {
-        int index = -1;
-        for (int i = 0; i < sizeofTest; i++) {
-            int count = 0;
-            for (int j = 0; j < sizeofTest; j++) {
-                if (test2[i] == test2[j]) {
-                    count++;
-                }
-            }
-            if (count < 2) {
-                index = i;
-            }
-        }
-        System.out.println(index);
-        int[] temp = test2;
-        test2 = new int[4];
-        int indexCount = 0;
-        for (int i = 0; i < sizeofTest; i++) {
-            if (i != index) {
-                test2[indexCount] = temp[i];
-                indexCount++;
-            }
-        }
-    }
-
-    public void test3CheckArray() {
-        int index = -1;
-        for (int i = 0; i < sizeofTest; i++) {
-            int count = 0;
-            for (int j = 0; j < sizeofTest; j++) {
-                if (test3[i] == test3[j]) {
-                    count++;
-                }
-            }
-            if (count < 2) {
-                index = i;
-            }
-        }
-        System.out.println(index);
-        int[] temp = test3;
-        test3 = new int[4];
-        int indexCount = 0;
-        for (int i = 0; i < sizeofTest; i++) {
-            if (i != index) {
-                test3[indexCount] = temp[i];
-                indexCount++;
-            }
-        }
-    }
-
-    public void showArray() {
-        System.out.println(Arrays.toString(test1));
-        System.out.println(Arrays.toString(test2));
-        System.out.println(Arrays.toString(test3));
+        System.out.println(Arrays.toString(test));
     }
 
     public static void main(String[] args) {
@@ -99,9 +45,8 @@ public class Day03302중복안된숫자제거 {
         // 1) {1,2,1,2}
         // 2) {4,4}
         // 3) {1,1,1,1}
-        day03302.test1CheckArray();
-        day03302.test2CheckArray();
-        day03302.test3CheckArray();
-        day03302.showArray();
+        day03302.testCheckArray(day03302.test1);
+        day03302.testCheckArray(day03302.test2);
+        day03302.testCheckArray(day03302.test3);
     }
 }
